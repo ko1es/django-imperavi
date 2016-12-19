291ace1f759421071e8b3fe832188fdf8b91b4b9
Nikita Kolesnikov <koles.web@gmail.com>
2016-12-19 20:26:23 +0700

    flake8 for widget.py

--- imperavi/widget.py
+++ imperavi/widget.py
@@ -14,22 +14,29 @@ IMPERAVI_SETTINGS = getattr(settings, 'IMPERAVI_CUSTOM_SETTINGS', {})
 
 
 class ImperaviWidget(Textarea):
+    """Imperavi extended widget."""
 
     def __init__(self, *args, **kwargs):
+        """Initial method."""
         self.upload_path = kwargs.pop('upload_path', UPLOAD_PATH)
         self.imperavi_settings = IMPERAVI_SETTINGS
         super(ImperaviWidget, self).__init__(*args, **kwargs)
 
     def render(self, name, value, attrs=None):
+        """Render  method."""
         if value is None:
             value = ''
         final_attrs = self.build_attrs(attrs, name=name)
         field_id = final_attrs.get('id')
         self.imperavi_settings.update({
-            'imageUpload': reverse('imperavi-upload-image', kwargs={'upload_path': self.upload_path}),
-            'imageGetJson': reverse('imperavi-get-json', kwargs={'upload_path': self.upload_path}),
-            'fileUpload': reverse('imperavi-upload-file', kwargs={'upload_path': self.upload_path}),
-            'linkFileUpload': reverse('imperavi-upload-link-file', kwargs={'upload_path': self.upload_path}),
+            'imageUpload': reverse('imperavi-upload-image',
+                                   kwargs={'upload_path': self.upload_path}),
+            'imageGetJson': reverse('imperavi-get-json',
+                                    kwargs={'upload_path': self.upload_path}),
+            'fileUpload': reverse('imperavi-upload-file',
+                                  kwargs={'upload_path': self.upload_path}),
+            'linkFileUpload': reverse('imperavi-upload-link-file',
+                                      kwargs={'upload_path': self.upload_path})
         })
         imperavi_settings = json.dumps(self.imperavi_settings)
         return mark_safe(u"""
@@ -46,9 +53,8 @@ class ImperaviWidget(Textarea):
                 );
             </script>
             """ % {
-                'attrs': flatatt(final_attrs),
-                'value': conditional_escape(force_unicode(value)),
-                'id': field_id,
-                'imperavi_settings': imperavi_settings,
-            }
-        )
+            'attrs': flatatt(final_attrs),
+            'value': conditional_escape(force_unicode(value)),
+            'id': field_id,
+            'imperavi_settings': imperavi_settings,
+        })
